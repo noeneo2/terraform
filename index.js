2dc4639aae596b9a61baf596a235d3eb79587f2e
noeneo2 <noelia.pantigoso@neo.com.pe>
2026-02-18 00:18:29 -0500

    Update response message in index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const http = require("http");
 const port = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
-  res.end("Hello from Cloud Run!");
+  res.end("Hello from Cloud Run asdfghj!");
 }).listen(port);
